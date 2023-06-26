6445a19dc7dd1cdad4ec3a4c105c0e5d285be346
alexfhernandez26 <alexfhernandez26@gmail.com>
2023-06-26 16:52:45 -0400

    Configuracion del Swagger en la clase program para que aparezca el Authorize en el swagger

--- WepApiAutores/Program.cs
+++ WepApiAutores/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
 using WepApiAutores;
@@ -42,7 +43,33 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c => 
+     {
+         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Wep Api Autores", Version = "v1" });
+     c.AddSecurityDefinition("Bearer",new  OpenApiSecurityScheme
+         {
+            Name = "Authorization",
+            Type=SecuritySchemeType.ApiKey,
+            Scheme="Bearer",
+            BearerFormat="Jwt",
+            In=ParameterLocation.Header
+         });
+         c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+           new string[]{}
+        }
+    });
+     });
+
 
 
 var app = builder.Build();
